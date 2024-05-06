b4fe15dc057bcbbfd26636ac7b61437db58adaca
brnomosa <omosabria99@gmail.com>
2024-05-06 14:41:28 +0300

    kk

---
+++ README.md
@@ -0,0 +1,18 @@
+#My Personal Website
+Description
+Website features are clean and interface designed to make tracking your avilability information for  easy and convenient.
+
+Access of the website from any device, including desktop computers, laptops, tablets, and smartphones. Whether you're at home, at work, or on the go, you can easily track any of your information  progress wherever you are.
+
+
+
+
+#Technology used
+HTML
+
+CSS
+
+JavaScript
+
+#Author information
+written by @omosa
---
+++ script.js
@@ -0,0 +1,25 @@
+function fetchDrinks(){
+    fetch("	http://api.tvmaze.com/search/shows?q=golden girls")
+    .then(res => res.json())
+    .then(data => 
+        console.log(data))
+    }
+    if (data.data) {
+   
+        Object.values(data.data).forEach(information => {
+         
+            add(gadgets);
+        });
+    } else {
+        console.error("No drinks found in the data");
+    }
+      
+    <div class="card">
+  <h2>Woody</h2>
+  <img src="[toy_image_url]" class="toy-avatar" />
+  <p>4 Likes</p>
+  <button class="like-btn" id="[toy_id]">Like ❤️</button>
+</div>
+
+    
+
